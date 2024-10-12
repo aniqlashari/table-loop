668a624c70ce49e5e7817334f2f99fe74241e73c
aniqlashari <mahaqamber3@gmail.com>
2024-10-12 20:21:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let name1 =+ prompt ("enter any number to genrate table")
+
+for ( index = 1; index <= name1; index++){
+
+ 
+ for ( k = 1; k <= 10; k++) {
+    
+   let name2 = document.write(name1+" X " + k + " = "+ k*name1+"<br />")
+
+   }
+
+for ( j = 1; j <= 1; j++) {
+ 
+   document.write("<br />")
+
+    
+}
+
+}
